c0aa74765b375336ed7f9f0ff207d87481edf4fb
Mohammed Mazin <muhammedmazin38@gmail.com>
2023-05-12 23:34:11 +0530

    initialized flutterfire and added web app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:divv/Authentication/Auth.dart';
+import 'package:divv/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
